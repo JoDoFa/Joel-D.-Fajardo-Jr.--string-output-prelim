c25107ce6c4aacfd24acaeb3ae040c0115986a05
JoDoFa <joel_fajardo@rcc.edu.ph>
2024-09-28 10:06:12 +0800

    first

---
+++ stringvalue/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const message = ["I'm Joel D. Fajardo"];
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
